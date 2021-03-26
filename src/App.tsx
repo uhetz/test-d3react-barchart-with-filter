6ab6d85e172e72574507f04d4f97e18b2e6f37dd
uhetz <hermhetzel@gmail.com>
2021-03-26 10:25:12 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import { TAppState } from './types/AppTypes';
+
+class App extends React.Component<{}, TAppState> {
+  render() {
+    return(
+      <div></div>
+    )
+  }
+}
+
+export default App;
